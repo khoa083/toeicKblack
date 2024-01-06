f46d3bb82480789ea2929bfdf9cc4ccdb95e9b84
khoa083 <minhkhoa065@gmail.com>
2024-01-07 00:02:56 +0700

    done 70 percent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 dependencies {
 
     val lifecycle_version = "2.6.2"
-    val nav_version = "2.7.5"
+    val nav_version = "2.7.6"
 
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
@@ -61,7 +61,7 @@ dependencies {
 //    implementation("androidx.core:core-ktx:2.2.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -88,8 +88,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    implementation("com.google.dagger:hilt-android:2.49")
-    kapt("com.google.dagger:hilt-compiler:2.49")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
 
 //    implementation 'com.airbnb.android:lottie:3.4.0'
     implementation("com.facebook.shimmer:shimmer:0.5.0")
